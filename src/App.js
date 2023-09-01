62095f0450216c0623f3e9dfcb59256666363fc2
canyapalak <canyapalak@gmail.com>
2023-09-01 15:27:31 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,75 @@
-import logo from './logo.svg';
-import './App.css';
+import { Link } from "react-router-dom";
+import "./App.css";
 
 function App() {
+  const exercises = [
+    {
+      name: "Exercise 1",
+      src: "/exercise-1",
+      definition: "Create a button which increases the number by one",
+    },
+    {
+      name: "Exercise 2",
+      src: "/exercise-2",
+      definition: "Create a toggle to show 'On' and 'Off' text",
+    },
+    {
+      name: "Exercise 3",
+      src: "/exercise-3",
+      definition: "Give a warning if an input is not minimum 6 characters",
+    },
+    {
+      name: "Exercise 4",
+      src: "/exercise-4",
+      definition: "Fetch an API and show data",
+    },
+    {
+      name: "Exercise 5",
+      src: "/exercise-5",
+      definition: "Update the parent string",
+    },
+    {
+      name: "Exercise 6",
+      src: "/exercise-6",
+      definition: "Make a calculator to sum two numbers",
+    },
+    {
+      name: "Exercise 7",
+      src: "/exercise-7",
+      definition: "Search a name in an array and show it",
+    },
+    {
+      name: "Exercise 8",
+      src: "/exercise-8",
+      definition: "Sum even numbers in an array",
+    },
+    {
+      name: "Exercise 9",
+      src: "/exercise-9",
+      definition: "Show a list of items with a button to remove each",
+    },
+    {
+      name: "Exercise 10",
+      src: "/exercise-10",
+      definition: "Show a list of items with remove and add function ",
+    },
+  ];
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2 className="App-header">React with Typescript - Exercises</h2>
+      <div className="exercise-wrapper">
+        {exercises.map((exercise) => (
+          <div key={exercise.name} className="exercise">
+            <p className="exercise-title">
+              <Link to={exercise.src} target="_blank">
+                {exercise.name}
+              </Link>
+            </p>
+            <p className="exercise-definition">{exercise.definition}</p>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
---
+++ src/Exercise1.js
@@ -0,0 +1,9 @@
+import "./App.css";
+
+export default function Exercise1() {
+  return (
+    <div>
+      <p>Test Test Exercise 1</p>
+    </div>
+  );
+}

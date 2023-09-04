962e3eb9298be5d166263725a5edace52c8cdf7e
canyapalak <canyapalak@gmail.com>
2023-09-04 15:43:54 +0200

    two more exercises added

--- src/views/Exercise3.tsx
+++ src/views/Exercise3.tsx
@@ -1,10 +1,77 @@
+import { Link } from "react-router-dom";
 import "../App.css";
-import React from "react";
+import React, { ChangeEvent, useState } from "react";
 
 export default function Exercise3() {
+  const [isValueShort, setIsValueShort] = useState<boolean>(false);
+
+  function handleInputChange(event: ChangeEvent<HTMLInputElement>) {
+    const value = event.target.value;
+    if (value.length <= 5) {
+      setIsValueShort(true);
+    } else {
+      setIsValueShort(false);
+    }
+  }
+
   return (
-    <div>
-      <p>Test Test This is a new page</p>
+    <div className="App">
+      <Link to="/">Home</Link>
+      <h3 className="App-header">
+        Exercise 3 - Give a warning if an input is not minimum 6 characters
+      </h3>
+      <p className="ex-header">Demo:</p>
+      <div className="imp">
+        <input onChange={handleInputChange}></input>
+        {isValueShort && <p>Value should be minimum 6 characters</p>}
+      </div>
+      <hr className="line" />
+      <div>
+        <p className="ex-header">Code:</p>
+        <div className="code-box">
+          <code>
+            {`export function App() {`}
+            <br />
+            {``}
+            <br />
+            {`const [isValueShort, setIsValueShort] = useState<boolean>(false);`}
+            <br />
+            {``}
+            <br />
+            {`function handleInputChange(event) {`}
+            <br />
+            {`const value = event.target.value;`}
+            <br />
+            {`if (value.length <= 5) {`}
+            <br />
+            {`setIsValueShort(true);`}
+            <br />
+            {`} else {`}
+            <br />
+            {`setIsValueShort(false);`}
+            <br />
+            {`}}`}
+            <br />
+            {``}
+            <br />
+            {`return (`}
+            <br />
+            {`<>`}
+            <br />
+            {`<input onChange={handleInputChange}></input>`}
+            <br />
+            {`{isValueShort && (`}
+            <br />
+            {`<p>Value should be minimum 6 characters</p>`}
+            <br />
+            {`)}`}
+            <br />
+            {`</>`}
+            <br />
+            {`)}`}
+          </code>
+        </div>
+      </div>
     </div>
   );
 }
--- src/views/Exercise4.tsx
+++ src/views/Exercise4.tsx
@@ -1,10 +1,122 @@
+import { Link } from "react-router-dom";
 import "../App.css";
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 export default function Exercise4() {
+  interface Character {
+    id: number;
+    name: string;
+  }
+
+  const [data, setData] = useState<Character[]>([]);
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    try {
+      const response = await fetch("https://rickandmortyapi.com/api/character");
+      const responseData = await response.json();
+      setData(responseData.results);
+    } catch (error) {
+      console.log("Error:", error);
+    }
+  };
   return (
-    <div>
-      <p>Test Test This is a new page</p>
+    <div className="App">
+      <Link to="/">Home</Link>
+      <h3 className="App-header">Exercise 4 - Fetch an API and show data</h3>
+      <p className="ex-header">Demo:</p>
+      <div className="imp">
+        <div>
+          <h3>Data</h3>
+          <ul>
+            {data.map((item) => (
+              <li key={item.id} id="list">
+                {item.name}
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
+      <hr className="line" />
+      <div>
+        <p className="ex-header">Code:</p>
+        <div className="code-box">
+          <code>
+            {`export function App() {`}
+            <br />
+            {``}
+            <br />
+            {`interface Character {`}
+            <br />
+            {`id: number;`}
+            <br />
+            {`name: string;`}
+            <br />
+            {`}`}
+            <br />
+            {``}
+            <br />
+            {`const [data, setData] = useState<Character[]>([]);`}
+            <br />
+            {``}
+            <br />
+            {`useEffect(() => {`}
+            <br />
+            {`fetchData();`}
+            <br />
+            {`}, []);`}
+            <br />
+            {``}
+            <br />
+            {`const fetchData = async () => {`}
+            <br />
+            {`try {`}
+            <br />
+            {`const response = await fetch('https://rickandmortyapi.com/api/character');`}
+            <br />
+            {`const responseData = await response.json();`}
+            <br />
+            {`setData(responseData.results);`}
+            <br />
+            {`} catch (error) {`}
+            <br />
+            {`console.log('Error:', error);`}
+            <br />
+            {`}};`}
+            <br />
+            {``}
+            <br />
+            {`console.log(data)`}
+            <br />
+            {``}
+            <br />
+            {`return (`}
+            <br />
+            {`<div>`}
+            <br />
+            {`<h2>Data</h2>`}
+            <br />
+            {`<ul>`}
+            <br />
+            {`{data.map((item) => (`}
+            <br />
+            {`<li key={item.id} id="list">{item.name}</li>`}
+            <br />
+            {`))}`}
+            <br />
+            {`</ul>`}
+            <br />
+            {`</div>`}
+            <br />
+            {`);`}
+            <br />
+            {`}`}
+          </code>
+        </div>
+      </div>
     </div>
   );
 }

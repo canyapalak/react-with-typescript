d69ee984ede138426b503f2fd1c9e53767d81289
canyapalak <canyapalak@gmail.com>
2023-09-05 16:07:59 +0200

    two more exercises

--- src/views/Exercise7.tsx
+++ src/views/Exercise7.tsx
@@ -1,10 +1,98 @@
+import { Link } from "react-router-dom";
 import "../App.css";
-import React from "react";
+import React, { useState } from "react";
 
 export default function Exercise7() {
+  const [searchQuery, setSearchQuery] = useState<string>("");
+  const users: string[] = ["can", "sebastian", "julia", "marco", "lisa"];
+
+  function handleChange(e: React.ChangeEvent<HTMLInputElement>) {
+    setSearchQuery(e.target.value);
+  }
+
+  const filteredUsers: string[] = users.filter((user) =>
+    user.includes(searchQuery.toLowerCase())
+  );
+
   return (
-    <div>
-      <p>Test Test This is a new page</p>
+    <div className="App">
+      <Link to="/">Home</Link>
+      <h3 className="App-header">
+        Exercise 7 - Search a name in an array and show it
+      </h3>
+      <p className="ex-header">Demo:</p>
+      <div className="imp">
+        <input type="text" onChange={handleChange}></input>
+        {filteredUsers.length === 0 ? (
+          <p>No users found!</p>
+        ) : (
+          <ul>
+            {filteredUsers.map((user, index) => (
+              <li key={index}>{user}</li>
+            ))}
+          </ul>
+        )}
+      </div>
+      <hr className="line" />
+      <div>
+        <p className="ex-header">Code:</p>
+        <div className="code-box">
+          <code>
+            {`export function App() {`}
+            <br />
+            {`const [searchQuery, setSearchQuery] = useState<string>("")`}
+            <br />
+            {`const users:string[] = ["can", "sebastian", "julia", "marco", "lisa"]`}
+            <br />
+            {``}
+            <br />
+            {`function handleChange(e) {`}
+            <br />
+            {`setSearchQuery(e.target.value);`}
+            <br />
+            {`};`}
+            <br />
+            {``}
+            <br />
+            {`const filteredUsers: string[] = users.filter(user =>`}
+            <br />
+            {`user.includes(searchQuery.toLowerCase())`}
+            <br />
+            {`);`}
+            <br />
+            {``}
+            <br />
+            {`console.log(filteredUsers)`}
+            <br />
+            {`return (`}
+            <br />
+            {`<>`}
+            <br />
+            {`<input type="text" onChange={handleChange}></input>`}
+            <br />
+            {`{filteredUsers.length === 0? (`}
+            <br />
+            {`<p>"No users found!"</p>`}
+            <br />
+            {`) : (`}
+            <br />
+            {`<ul>`}
+            <br />
+            {`{filteredUsers.map((user, index) =>`}
+            <br />
+            {`<li key={index}>{user}</li>)}`}
+            <br />
+            {`</ul>`}
+            <br />
+            {`)}`}
+            <br />
+            {`</>)`}
+            <br />
+            {`}`}
+            <br />
+          </code>
+        </div>
+      </div>
     </div>
   );
 }
--- src/views/Exercise8.tsx
+++ src/views/Exercise8.tsx
@@ -1,10 +1,75 @@
+import { Link } from "react-router-dom";
 import "../App.css";
 import React from "react";
 
 export default function Exercise8() {
+  function getEvenSum(numbers: number[]): number {
+    let sum = 0;
+    for (const num of numbers) {
+      if (num % 2 === 0) {
+        sum += num;
+      }
+    }
+    return sum;
+  }
+  const numbersArray = [1, 3, 4, 6, 8, 11, 12];
+  const evenNumbersSum = getEvenSum(numbersArray);
+
   return (
-    <div>
-      <p>Test Test This is a new page</p>
+    <div className="App">
+      <Link to="/">Home</Link>
+      <h3 className="App-header">Exercise 8 - Sum even numbers in an array</h3>
+      <p className="ex-header">Demo:</p>
+      <div className="imp">
+        <p>Numbers array: [1, 3, 4, 6, 8, 11, 12]</p>
+        <p>The sum of even numbers are: {evenNumbersSum}</p>
+      </div>
+      <hr className="line" />
+      <div>
+        <p className="ex-header">Code:</p>
+        <div className="code-box">
+          <code>
+            {`export function App() {`}
+            <br />
+            {``}
+            <br />
+            {`function getEvenSum(numbers:number[]): number {`}
+            <br />
+            {`let sum = 0`}
+            <br />
+            {`for (const num of numbers) {`}
+            <br />
+            {`if (num % 2 === 0) {`}
+            <br />
+            {`sum += num;`}
+            <br />
+            {`}}`}
+            <br />
+            {`return sum`}
+            <br />
+            {`}`}
+            <br />
+            {``}
+            <br />
+            {`const evenNumbersSum = getEvenSum([1, 3, 4, 6, 8, 10, 11, 15]);`}
+            <br />
+            {``}
+            <br />
+            {`return (`}
+            <br />
+            {`<>`}
+            <br />
+            {`<p>Numbers array: [1, 3, 4, 6, 8, 11, 12]</p>`}
+            <br />
+            {`<p>The sum of even numbers are: {evenNumbersSum}</p>`}
+            <br />
+            {`</>`}
+            <br />
+            {`)}`}
+            <br />
+          </code>
+        </div>
+      </div>
     </div>
   );
 }
